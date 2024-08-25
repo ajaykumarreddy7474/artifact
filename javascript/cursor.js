69a825ead25873ed937ad642575d287f9db96fa7
Kethireddy Ajay kumar Reddy <ajaykumarreddy7474@gmail.com>
2024-08-25 21:36:45 +0530

    5th

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -3,3 +3,16 @@ document.addEventListener('mousemove', (e) => {
 circleCursor.style.left = e.clientX+25+`px`;
 circleCursor.style.top = e.clientY+30+`px`
 });
+document.addEventListener('mouseover', (e) => {
+    if (e.target.tagName === 'DIV') {
+        circleCursor.style.width = '40px';  // Increase width
+        circleCursor.style.height = '40px'; // Increase height
+    }
+});
+
+document.addEventListener('mouseout', (e) => {
+    if (e.target.tagName === 'DIV') {
+        circleCursor.style.width = '120px';  // Reset width
+        circleCursor.style.height = '120px'; // Reset height
+    }
+});
